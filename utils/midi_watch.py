9bf54412d1649a37ed238c6faf8f162d0da4910a
my name <you@example.com>
2019-07-06 17:11:15 +0100

    fully working and tuned noisily

--- utils/midi_watch.py
+++ utils/midi_watch.py
@@ -1,25 +1,11 @@
 import mido
 from pprint import pprint
+import miditools as mt
 
-midi_devices = mido.get_input_names()
-port_dict = {}
-
-while True:
-    pprint([ (i, midi_devices[i]) for i in range(len(midi_devices))] )
-    user_input = input('select midi device number: ')
-    print(midi_devices)
-    if user_input is "" or midi_devices is []:
-        break
-    try:
-        which_device = int(user_input)
-        device_name = midi_devices.pop(which_device)
-        inport = mido.open_input(device_name)
-        port_dict[device_name] = inport
-    except:
-        print("please enter an into or nothing to continue")
+midi_ports = mt.user_midi()
 
 while 1:
-    for device, midi_port in port_dict.items():
+    for device, midi_port in midi_ports.items():
 
         # if two cc messages with the same control and channel have come then only append most recent
         for msg in midi_port.iter_pending():
